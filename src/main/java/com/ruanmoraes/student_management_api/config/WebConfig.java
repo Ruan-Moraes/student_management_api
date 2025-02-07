016028394effa1fa061d76af971eb704444beffe
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2025-02-07 19:03:04 -0300

    raw: configura cors e altera application.properties

--- src/main/java/com/ruanmoraes/student_management_api/config/WebConfig.java
+++ src/main/java/com/ruanmoraes/student_management_api/config/WebConfig.java
@@ -1,4 +1,18 @@
 package com.ruanmoraes.student_management_api.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+    }
 }
