a8b32bcee1832384756138f7648b6b387fe4d368
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2025-03-03 18:12:19 -0300

    --amend

--- src/main/java/com/ruanmoraes/student_management_api/controllers/StudentController.java
+++ src/main/java/com/ruanmoraes/student_management_api/controllers/StudentController.java
@@ -4,7 +4,6 @@ import com.ruanmoraes.student_management_api.dtos.request.StudentRequestDTO;
 import com.ruanmoraes.student_management_api.dtos.response.StudentResponseDTO;
 import com.ruanmoraes.student_management_api.services.StudentService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,42 +22,42 @@ public class StudentController {
     public ResponseEntity<?> getAllStudents() {
         List<StudentResponseDTO> students = studentService.getAllStudents();
 
-        return ResponseEntity.status(HttpStatus.OK).body(students);
+        return ResponseEntity.status(200).body(students);
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<?> getStudentById(@PathVariable Long id) {
+    public ResponseEntity<StudentResponseDTO> getStudentById(@PathVariable Long id) {
         StudentResponseDTO student = studentService.getStudentById(id);
 
-        return ResponseEntity.status(HttpStatus.OK).body(student);
+        return ResponseEntity.status(200).body(student);
     }
 
-    @PostMapping(path = "/create", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> createStudent(@Valid @RequestBody StudentRequestDTO studentRequestDTO) {
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<StudentResponseDTO> createStudent(@Valid @RequestBody StudentRequestDTO studentRequestDTO) {
         StudentResponseDTO student = studentService.createStudent(studentRequestDTO);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(student);
+        return ResponseEntity.status(201).body(student);
     }
 
 
-    @PutMapping(value = "/update/{id}", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> updateById(@PathVariable Long id, @Valid @RequestBody StudentRequestDTO studentRequestDTO) {
-        StudentResponseDTO student = studentService.updateById(id, studentRequestDTO);
+    @PutMapping(value = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<StudentResponseDTO> updateById(@Valid @RequestBody StudentRequestDTO studentRequestDTO) {
+        StudentResponseDTO student = studentService.updateById(studentRequestDTO);
 
-        return ResponseEntity.status(HttpStatus.OK).body(student);
+        return ResponseEntity.status(200).body(student);
     }
 
-    @GetMapping("/low-frequency/{frequency}")
-    public ResponseEntity<?> listStudentsByLowFrequency(@PathVariable Double frequency) {
+    @GetMapping("/low-frequency")
+    public ResponseEntity<List<StudentResponseDTO>> listStudentsByLowFrequency(@RequestParam("frequency_below") Double frequency) {
         List<StudentResponseDTO> students = studentService.listStudentsByLowFrequency(frequency);
 
-        return ResponseEntity.status(HttpStatus.OK).body(students);
+        return ResponseEntity.status(200).body(students);
     }
 
-    @DeleteMapping(value = "/delete/{id}", produces = "application/json")
-    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         studentService.deleteById(id);
 
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(204).build();
     }
 }
